fffc00c07ae5035fdb5df7e5dcd128a3093b4e5e
Alexander Gabriel <alexander.gabriel@digital-infinity.de>
2021-02-20 18:17:46 +0100

    added tileset

---
+++ tilesets/modern_interiors_ukg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="modern_interiors_ukg" tilewidth="32" tileheight="32" tilecount="8082" columns="18">
+ <image source="../maps/crea/assets/modern_interiors_ukg.png" width="600" height="14368"/>
+</tileset>
